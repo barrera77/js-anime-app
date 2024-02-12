a7e7931d58e933b7d2eaa982cdc5d4ff6c4fbbea
Manuel Alva <barrera_ml@hotmail.com>
2024-02-11 22:21:12 -0700

    Fetched Pagination info from API

--- js/index.js
+++ js/index.js
@@ -1,24 +1,68 @@
+import ApiClient from "../services/api-client.js";
+
 const url = "https://api.jikan.moe/v4/top/anime?bypopularity/pictures";
+const endpoint = "";
 const animeGrid = document.querySelector("#anime-grid");
 const darkModeSwitch = document.querySelector("#dark-mode-switch");
 const playButton = document.querySelector("#play-button");
+const currentPageNumber = document.querySelector("#current-page-number");
+const lastPageNumber = document.querySelector("#last-page-number");
 
+const nextPageNumber = document.querySelector("#next-page");
+nextPageNumber.addEventListener("click", onHandleNextPage);
+
+const previousPageNumber = document.querySelector("#previous-page-number");
+previousPageNumber.addEventListener("click", onHandlePreviousPage);
+
+//Toggle dark mode
 darkModeSwitch.addEventListener("change", () => {
   document.querySelector("html").classList.toggle("dark");
+
+  document.querySelector("#login-logo").style.filter = "invert(100%)";
 });
 
-fetchAnimeData();
+//Query the API for anime data
+const apiclient = new ApiClient("?bypopularity");
+const animeList = await apiclient.getAnimeData(
+  Number(currentPageNumber.textContent)
+);
 
+//Create variables for the current page counters
+let nextPage = animeList.pagination.current_page;
+let lastPage = animeList.pagination.last_visible_page;
+
+//Initialize counters on the paginator
+lastPageNumber.textContent = lastPage;
+currentPageNumber.textContent = nextPage;
+
+async function onHandleNextPage(e) {
+  e.preventDefault();
+
+  const hasNextPage = animeList.pagination.has_next_page;
+
+  if (hasNextPage) {
+    nextPage += 1;
+    console.log(nextPage);
+    currentPageNumber.textContent = nextPage;
+    lastPageNumber.textContent = lastPage -= 1;
+  }
+}
+
+function onHandlePreviousPage(e) {
+  e.preventDefault();
+
+  if (nextPage > 1) {
+    nextPage -= 1;
+    console.log(nextPage);
+    currentPageNumber.textContent = nextPage;
+    lastPageNumber.textContent = lastPage += 1;
+  }
+}
+
+//Fetch the anime data and display it
 async function fetchAnimeData() {
   try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error("Could not fetch a resource");
-    }
-    const data = await response.json();
-
-    data.data.forEach((anime) => {
+    animeList.data.forEach((anime) => {
       //Access the anime URLs and create img elements
       const imgUrl = anime.images.jpg.image_url;
       const animeTitle = anime.title;
@@ -45,8 +89,47 @@ async function fetchAnimeData() {
       </div>`;
       }
     });
-    console.log(data);
   } catch (error) {
-    console.error("Error: " + error);
+    console.error("Error fetching anime data: ", error);
   }
 }
+
+fetchAnimeData();
+//getAnimePages(1);
+
+//Set paginator initial values
+/* async function getAnimePages(page) {
+  const animePages = await apiclient.getAnimeData(page);
+  const currentPage = animePages.pagination.current_page;
+  const lastVisiblePage = animePages.pagination.last_visible_page;
+
+  currentPageNumber.textContent = currentPage;
+  lastPageNumber.textContent = lastVisiblePage;
+} */
+
+/* async function getAnimePages(page) {
+  const animePages = await apiclient.getAnimeData(page);
+  const currentPage = animePages.pagination.current_page;
+  const lastVisiblePage = animePages.pagination.last_visible_page;
+
+  return { currentPage, lastVisiblePage };
+}
+ */
+//Create the next page
+async function loadNextPage(page) {
+  animeGrid.innerHTML = ""; //clear cards
+  await fetchAnimeData(page); //Load next set of anime cards
+}
+
+//-------------------TESTER--------------------------------------------
+/* const apiclient = new ApiClient("?bypopularity");
+async function fetchAnimeData(page) {
+  try {
+    const animeList = await apiclient.getAnimeData(page);
+
+    console.log(animeList.pagination.current_page);
+  } catch (error) {
+    console.error(error);
+  }
+}
+fetchAnimeData(5); */
---
+++ services/api-client.js
@@ -0,0 +1,25 @@
+const url = "https://api.jikan.moe/v4/top/anime";
+
+/* Class to query the API to get data and pagination info filtered by top-anime
+https://api.jikan.moe/v4/top/anime?page=${page} */
+class ApiClient {
+  constructor(endpoint) {
+    this.url = url;
+    this.endpoint = endpoint;
+  }
+  async getAnimeData(page) {
+    try {
+      const response = await fetch(`${this.url}${this.endpoint}&page=${page}`);
+
+      if (!response.ok) {
+        throw new Error("Incorrect network response");
+      }
+
+      const { data, pagination } = await response.json();
+      return { data, pagination };
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+export default ApiClient;
